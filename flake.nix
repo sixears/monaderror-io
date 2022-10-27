ea28e322585e23637399247d09b697553241fc0b
Mr. Fluffy <git@sixearsm>
2022-10-27 08:43:13 +0000

    add flake; use ghc-8.10.7 for tfmt

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "An AsIOError class for compound errors, and some MonadError handling utilities";
+
+  inputs = {
+    nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.0";
+
+    base0.url         = "github:sixears/base0/r0.0.4.1";
+    has-callstack.url = "github:sixears/has-callstack/r1.0.1.2";
+    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.1";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, build-utils
+            , base0, has-callstack, more-unicode }:
+    build-utils.lib.hOutputs self nixpkgs "monaderror-io" {
+      deps = {
+        inherit base0 has-callstack more-unicode;
+      };
+      ghc = p: p.ghc8107; # for tfmt
+    };
+}
