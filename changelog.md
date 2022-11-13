468503d604bfee46d8c68f0dff058ea5902a729d
Mr. Fluffy <git@sixearsm>
2022-11-13 06:55:18 +0000

    fix fixed-package-name typo in flake-build-utils

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+1.2.5.9 2022-11-13
+==================
+- fix fixed-package-name typo in flake-build-utils
+
 1.2.5.8 2022-11-03
 ==================
 - remove redundant "output" flake-utils
--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.6";
+    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.10";
 
-    base0.url         = "github:sixears/base0/r0.0.4.4";
-    has-callstack.url = "github:sixears/has-callstack/r1.0.1.9";
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.6";
+    base0.url         = "github:sixears/base0/r0.0.4.5";
+    has-callstack.url = "github:sixears/has-callstack/r1.0.1.10";
+    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.7";
   };
 
   outputs = { self, nixpkgs, build-utils
