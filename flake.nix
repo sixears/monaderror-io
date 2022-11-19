4e998d33d2df7cc25f815162dae52cf5519020ba
Mr. Fluffy <git@sixearsm>
2022-11-19 06:39:29 +0000

    natural->0.0.1.13

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
     base0.url         = github:sixears/base0/r0.0.4.10;
-    has-callstack.url = github:sixears/has-callstack/r1.0.1.16;
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.17;
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
   };
 
